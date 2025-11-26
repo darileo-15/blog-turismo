2979f8871e4842b527a0e29a4d78e305ad4dc7a6
ema-munoz <ema.munoz@yavirac.edu.ec>
2025-11-26 15:11:29 -0500

    Diseño Header y Footer V1

--- src/public/js/loadComponents.js
+++ src/public/js/loadComponents.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
 	const header = document.getElementById("header-placeholder");
 	const footer = document.getElementById("footer-placeholder");
 
-	fetch("/blog-turismo/views/partials/header.html")
+	fetch("./views/partials/header.html")
 		.then((res) => res.text())
 		.then((html) => (header.innerHTML = html))
 		.catch((err) => console.error("Error cargando header:", err));
 
-	fetch("/blog-turismo/views/partials/footer.html")
+	fetch("./views/partials/footer.html")
 		.then((res) => res.text())
 		.then((html) => (footer.innerHTML = html))
 		.catch((err) => console.error("Error cargando footer:", err));

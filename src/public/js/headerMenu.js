39807d4b776c29bf4f3fd671e470676092a4a110
ema-munoz <ema.munoz@yavirac.edu.ec>
2025-12-01 11:04:08 -0500

    Diseño Header

--- src/public/js/headerMenu.js
+++ src/public/js/headerMenu.js
@@ -1,17 +1,3 @@
-/*function initHeaderMenu() {
-	const btn = document.getElementById("menu-btn");
-	const menu = document.getElementById("menu-links");
-
-	btn.addEventListener("click", () => {
-		if (menu.style.display === "block") {
-			menu.style.display = "none";
-		} else {
-			menu.style.display = "block";
-		}
-	});
-}
-*/
-
 function initHeaderMenu() {
 	const btn = document.getElementById("menu-btn");
 	const menu = document.getElementById("menu-links");

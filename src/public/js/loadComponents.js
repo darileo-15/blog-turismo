2961fba7635d90c0e195f8f1672007b5ba5d9389
ema-munoz <ema.munoz@yavirac.edu.ec>
2025-12-01 22:32:47 -0500

    Diseño Header

--- src/public/js/loadComponents.js
+++ src/public/js/loadComponents.js
@@ -1,29 +1,16 @@
-/*document.addEventListener("DOMContentLoaded", () => {
-	const header = document.getElementById("header-placeholder");
-	const footer = document.getElementById("footer-placeholder");
-
-	fetch("./src/views/partials/header.html")
-		.then((res) => res.text())
-		.then((html) => (header.innerHTML = html))
-		.catch((err) => console.error("Error cargando header:", err));
-
-	fetch("./src/views/partials/footer.html")
-		.then((res) => res.text())
-		.then((html) => (footer.innerHTML = html))
-		.catch((err) => console.error("Error cargando footer:", err));
-});
-*/
-
 document.addEventListener("DOMContentLoaded", () => {
 	const header = document.getElementById("header-placeholder");
 	const footer = document.getElementById("footer-placeholder");
 
-	fetch("./src/views/partials/header.html")
+	fetch("src/views/partials/header.html")
 		.then((res) => res.text())
-		.then((html) => (header.innerHTML = html))
+		.then((html) => {
+			header.innerHTML = html;
+			initHeaderMenu(); // 🔥 importante para activar el JS del menú
+		})
 		.catch((err) => console.error("Error cargando header:", err));
 
-	fetch("./src/views/partials/footer.html")
+	fetch("src/views/partials/footer.html")
 		.then((res) => res.text())
 		.then((html) => (footer.innerHTML = html))
 		.catch((err) => console.error("Error cargando footer:", err));
